ead801071a482098e31256703d07e8d7ae9aa7c3
SuhibIrsheidat <86198143+SuhibIrsheidat@users.noreply.github.com>
2023-10-15 21:39:03 +0200

    Update engine.js

--- engine.js
+++ engine.js
@@ -129,7 +129,7 @@ async function presentData() {
         } else if (priceChangeLast10m > 2.35 && priceChangeLast10m < 10.0) {
             cardBg = 'background-color: #366C1B; border-color: #85bb65'
         } else if (priceChangeLast10m >= 10) {
-            cardBg = 'background-color: #85bb65; border-color: #366C1B; transform: scale(1.1)'
+            cardBg = 'background-color: #366C1B; border-color: #85bb65; transform: scale(1.1)'
         }
 
         var candlesHtml = ``
